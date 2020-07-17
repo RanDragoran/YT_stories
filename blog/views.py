31239a4bf82ebd69fa4a1c3f133a069398c39444
Crisbern <tumangcris@gmail.com>
2020-07-18 00:59:11 +0800

    Added a blog entry

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import blog
 
 def blogs(request):
-    blogs_dictionary = {'navBar': 'blog'}
+    blogs = blog.objects.all()
+    latestDate = blog.objects.latest('blogDate')
+    blogs_dictionary = {'blogs': blogs, 'navBar': 'blog', 'date' : latestDate}
+
     return render(request, 'blog/blogs.html', blogs_dictionary)
